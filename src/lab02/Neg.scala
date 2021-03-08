48c6c2153e987d79112eacf38d2b4756f3734106
Marta Spadoni <marta.spadoni2@studio.unibo.it>
2021-03-08 11:51:58 +0100

    made neg work for generic predicates

--- src/lab02/Neg.scala
+++ src/lab02/Neg.scala
@@ -13,9 +13,21 @@ object Neg extends App{
   println(neg(isFoo)("pippo")) //true
   println(neg(isFoo)("foo")) //false
 
-  def negMethod (pred : String => Boolean) : String => Boolean = x => !pred(x)
+  def negMethod (predicate : String => Boolean) : String => Boolean = x => !predicate(x)
 
   val notE = negMethod(empty) //type: String => Boolean
   println(notE("foo") && !notE(""))
+
+  def genericNeg[A](predicate:A => Boolean) : A => Boolean = x => !predicate(x)
+
+  println("--- TEST GENERIC NEG ---")
+  //it has to still working with String to Boolean predicate
+  val genNotEmpty = genericNeg(empty) //type: String => Boolean
+  println(genNotEmpty("foo") && !genNotEmpty(""))
+  val greaterThanFive = (x:Int) => x > 5
+  val lessThanFive = genericNeg(greaterThanFive)
+  println(lessThanFive(3)) //true
+  println(lessThanFive(10)) //false
+
 
 }
