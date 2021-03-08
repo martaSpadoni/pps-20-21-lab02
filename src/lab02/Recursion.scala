66832854b4ffd2dbb7e3e50d7fe4b3a1aad147e2
Marta Spadoni <marta.spadoni2@studio.unibo.it>
2021-03-08 18:05:48 +0100

    Implemented fib function with tail recursion

---
+++ src/lab02/Recursion.scala
@@ -0,0 +1,17 @@
+package lab02
+
+object Recursion extends App{
+
+  def fib(n: Int): Int = {
+    @annotation.tailrec
+    def tailFib(fib: Int, prev: Int, n: Int):Int = n match {
+      case 0 => 0
+      case 1 => fib
+      case _ => tailFib(fib+prev, fib, n-1)
+    }
+    tailFib(1, 0, n)
+  }
+
+  println(fib(0), fib(1), fib(2), fib(3), fib(4), fib(5))
+
+}
---
+++ test/lab02/Functions2aTest.scala
@@ -0,0 +1,7 @@
+package lab02
+
+class Functions2aTest {
+
+
+
+}
