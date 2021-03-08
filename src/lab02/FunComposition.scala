b93f609fd0fdd89144fe07fdbb0e6693e7b8d06c
Marta Spadoni <marta.spadoni2@studio.unibo.it>
2021-03-08 12:44:40 +0100

    implements generic compose

--- src/lab02/FunComposition.scala
+++ src/lab02/FunComposition.scala
@@ -1,10 +1,20 @@
 package lab02
 
+
 object FunComposition extends App {
 
   def compose(f: Int => Int, g: Int => Int): Int => Int = x => f(g(x))
 
   println(compose(_-1, _*2)(5))
   println(compose(_+10,_/2)(2)) //11
+
+  def genericCompose[A,B,C](f: B => C, g: A => B): A => C = x => f(g(x))
+  val f = (x:Int) => x match {
+    case n if n >= 0 => "pos"
+    case _ => "neg"
+  }
+  val g = (x:Int) => x-2
+  println(genericCompose(f, g)(5))
+  println(genericCompose((x:Int) => x>10,(y:Int) => y*2 )(3))
 
 }
