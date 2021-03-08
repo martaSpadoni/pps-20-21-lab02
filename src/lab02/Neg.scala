a78277369e7972dbe8ef01dadd433f993257ea9a
Marta Spadoni <marta.spadoni2@studio.unibo.it>
2021-03-08 11:30:35 +0100

    Implemented neg function as lambda

---
+++ src/lab02/Neg.scala
@@ -0,0 +1,15 @@
+package lab02
+
+object Neg extends App{
+
+  val neg: (String => Boolean) => String => Boolean = predicate => x => !predicate(x)
+
+  val empty : String => Boolean = _ == ""
+  val notEmpty = neg(empty) //type: String => Boolean
+  println(notEmpty("foo"), notEmpty(""))
+  println(notEmpty("foo") && !notEmpty(""))
+
+  val isFoo = (str: String) => str == "foo"
+  println(neg(isFoo)("pippo")) //true
+  println(neg(isFoo)("foo")) //false
+}
