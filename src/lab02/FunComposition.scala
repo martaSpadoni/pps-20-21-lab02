bfb522dfffa32a224146810c0dabb15b6b2d10f8
Marta Spadoni <marta.spadoni2@studio.unibo.it>
2021-03-09 12:04:28 +0100

    refactoring FunComposition

--- src/lab02/FunComposition.scala
+++ src/lab02/FunComposition.scala
@@ -5,16 +5,18 @@ object FunComposition extends App {
 
   def compose(f: Int => Int, g: Int => Int): Int => Int = x => f(g(x))
 
-  println(compose(_-1, _*2)(5))
+  println(compose(_-1, _*2)(5)) //9
   println(compose(_+10,_/2)(2)) //11
 
   def genericCompose[A,B,C](f: B => C, g: A => B): A => C = x => f(g(x))
+
   val f = (x:Int) => x match {
     case n if n >= 0 => "pos"
     case _ => "neg"
   }
   val g = (x:Int) => x-2
-  println(genericCompose(f, g)(5))
-  println(genericCompose((x:Int) => x>10,(y:Int) => y*2 )(3))
+
+  println(genericCompose(f, g)(5)) //"pos"
+  println(genericCompose((x:Int) => x>10,(y:Int) => y*2 )(3)) //false
 
 }
