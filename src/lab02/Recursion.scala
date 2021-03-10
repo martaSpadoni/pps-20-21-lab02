31817e588333b1aff0222cc4c6a6e070a5a4351e
Marta Spadoni <marta.spadoni2@studio.unibo.it>
2021-03-10 10:58:21 +0100

    refactoring tailFib

--- src/lab02/Recursion.scala
+++ src/lab02/Recursion.scala
@@ -5,11 +5,11 @@ object Recursion extends App{
   def fib(n: Int): Int = {
     @annotation.tailrec
     def tailFib(fib: Int, prev: Int, n: Int):Int = n match {
-      case 0 => 0
-      case 1 => fib
+      case 0 => fib
       case _ => tailFib(fib+prev, fib, n-1)
     }
-    tailFib(1, 0, n)
+
+    tailFib(0, 1, n)
   }
 
   println(fib(0), fib(1), fib(2), fib(3), fib(4), fib(5)) //(0,1,1,2,3,5)
